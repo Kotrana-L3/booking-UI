b0e2cc808e7bb40a6a82f49f99aac35f9e2ff3b7
Jonathan Rajao <hei.tovomihaja@gmail.com>
2024-12-17 17:08:40 +0300

    feat: handle landing page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,23 @@
-
-export default function Home (){
-    return(
+import landingPic from "../pics/les_savons_frais_6757-500x334.jpg"
+import Image from "next/image"
+import "@/app/home/home.css"
+export default function Home() {
+    return (
         <main>
-           
+            <section className="relative mt-20 ">
+                <Image src={landingPic} alt="landingPic" className="w-8/12  absolute ml-12 -z-50"></Image>
+                <div className="uppercase z-50 absolute landingDescription">
+                    <h2 className="text-2xl">
+                        SAVONS ET BOUGIES BIOLOGIQUES ARTISANAUX
+                    </h2>
+                    <h1 className="text-7xl textWidth">
+                        COMME LA NATURE L'A PRÉVU
+                    </h1>
+                    <button className="font-light bg-yellow-900 text-white mt-10 px-12 py-2">
+                        Acheter
+                    </button>
+                </div>
+            </section>
         </main>
     )
 }
