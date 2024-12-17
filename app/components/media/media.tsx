fc95ff4144aaa0f360ed7460244d9652ac49005c
Jonathan Rajao <hei.tovomihaja@gmail.com>
2024-12-17 17:32:41 +0300

    feat: show media logo

---
+++ app/components/media/media.tsx
@@ -0,0 +1,13 @@
+import { TiSocialFacebook } from "react-icons/ti";
+import { IoLogoInstagram } from "react-icons/io";
+import { FaPinterestP } from "react-icons/fa";
+import "@/app/home/home.css"
+export default function Media() {
+    return (
+        <div className="media">
+            <TiSocialFacebook />
+            <IoLogoInstagram />
+            <FaPinterestP />
+        </div>
+    )
+}
